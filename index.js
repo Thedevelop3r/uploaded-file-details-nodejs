9294ccbe581ee51a0e1fe7e584310e01d51ba4f7
Bilal-Arain <bilalarain444@gmail.com>
2022-10-07 21:06:51 +0500

    v 1.0

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
-var express = require("express");
-var cors = require("cors");
+let express = require("express");
+let cors = require("cors");
 require("dotenv").config();
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
-
-
-
-var app = express();
+let app = express();
 app.use(fileUpload());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(cors());
 app.use("/public", express.static(process.cwd() + "/public"));
-
 app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
-
 app.post("/api/fileanalyse", (req, res) => {
-
   // console.log(req.body.msg);
   console.log(req.files.upfile);
-
   const uFile = req.files.upfile;
-
-
   res.json({
     name: uFile.name,
     type: uFile.mimetype,
